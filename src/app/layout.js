9987200157e13e1755f85d23ae8e7929503f701d
Samyak-J7 <sj21samyak@gmail.com>
2024-03-20 15:59:04 +0530

    Add @clerk/themes package and update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
+import Header from "@/components/header";
+import { dark } from '@clerk/themes';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,9 +12,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      baseTheme: dark
+    }}>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Header />
+      {children}
+      </body>
     </html>
     </ClerkProvider>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,7 @@
-'use client';
-
-import { Button } from "@/components/ui/button";
-import { SignIn } from "@clerk/clerk-react";
-import { SignedIn ,UserButton , SignInButton } from "@clerk/nextjs";
-import { SignedOut } from "@clerk/nextjs";
-import Image from "next/image";
-import Link from "next/link";
-
+import React from 'react'
 export default function Home() {
   return (
-    <div>
-    <SignedIn>
-      <UserButton afterSignOutUrl="/" />
-      <Button> 
-        <Link href="/draw" >Get Started</Link>
-      </Button>
-    </SignedIn> 
-    <SignedOut>
-      <SignInButton afterSignInUrl="/" >
-        <Button>Get Started</Button>
-      </SignInButton>
-    </SignedOut>   
-    
-    </div>
-    
+    <div>Welcome</div>    
     
   );
 }

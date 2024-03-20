81acc2cab6e9864a327b93ae0efc147eb049fe34
Samyak-J7 <sj21samyak@gmail.com>
2024-03-20 16:06:03 +0530

    Remove header component from layout.js and add header component to page.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
-import Header from "@/components/header";
 import { dark } from '@clerk/themes';
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +16,7 @@ export default function RootLayout({ children }) {
     }}>
     <html lang="en">
       <body className={inter.className}>
-      <Header />
+      
       {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
+import Header from '@/components/header';
 import React from 'react'
 export default function Home() {
   return (
-    <div>Welcome</div>    
+    <Header />  
     
   );
 }

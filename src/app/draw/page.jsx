4096e66223595c16c771790058e7c6294bc52e10
Samyak-J7 <sj21samyak@gmail.com>
2024-03-20 15:04:47 +0530

    Add user authentication and update button in page.js

---
+++ src/app/draw/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Draw = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Draw
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,20 @@
+'use client';
+
 import { Button } from "@/components/ui/button";
+import { SignIn } from "@clerk/clerk-react";
+import { SignedIn ,UserButton , SignInButton } from "@clerk/nextjs";
+import { SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Button>Get Started</Button>   
+    <div>
+    <SignedIn><UserButton afterSignOutUrl="/" /><Button> <Link href="/draw" >Get Started</Link> </Button></SignedIn> 
+    <SignedOut><SignInButton afterSignInUrl="/" ><Button>Get Started</Button></SignInButton></SignedOut>   
+    
+    </div>
+    
+    
   );
 }

ce65ba361564511619c2f2777091717642118106
Samyak-J7 <sj21samyak@gmail.com>
2024-03-20 14:42:07 +0530

    Add Button component and ClerkProvider

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>hello world</h1>
+    <Button>Get Started</Button>   
   );
 }
